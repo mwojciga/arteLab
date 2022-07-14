6b4a3ea6f5a0e0d24ff8e387e32a4c5e9e6d4a76
Maciej Wojciga <maciej.wojciga@contracted.pmi.com>
2022-07-14 12:20:27 +0200

    Migrate to GitHub

---
+++ dbfs.bat
@@ -0,0 +1,11 @@
+:: Starting script for shearrobot software.
+
+@ECHO OFF
+ECHO. 
+ECHO ############################################
+ECHO ##    Dynamic Blood Flow Simulator 2014   ##
+ECHO ############################################
+ECHO. 
+TYPE docs\readme.txt
+ECHO.
+java -jar dbfs.jar
---
+++ distrib/dbfs_1.0.0/readme.txt
@@ -0,0 +1,2 @@
+Version = 1.0.0
+Compiled = 2014.11.09 15:05:37 by Klaudia Trembecka, Maciej Wojciga
